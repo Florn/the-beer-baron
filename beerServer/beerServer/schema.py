e8112e7db404646642c79c7f6ae27c32d6c681cf
Florn <flapsnms@gmail.com>
2018-08-31 19:03:20 +0100

    Updated graphene classes as per 2.0 upgrade notes

--- beerServer/beerServer/schema.py
+++ beerServer/beerServer/schema.py
@@ -2,8 +2,12 @@ import graphene
 import gqlserver.schema
 
 
+class Mutation(gqlserver.schema.Mutation, graphene.ObjectType):
+    pass
+
+
 class Query(gqlserver.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- beerServer/gqlserver/schema.py
+++ beerServer/gqlserver/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter.fields import DjangoFilterConnectionField
 from . import models
+import json
 
 
 class MessageType(DjangoObjectType):
@@ -9,6 +10,27 @@ class MessageType(DjangoObjectType):
         model = models.Message
         filter_fields = {'message': ['icontains']}
         interfaces = (graphene.Node, )
+
+
+class CreateMessage(graphene.Mutation):
+    class Input:
+        message = graphene.String()
+
+    form_errors = graphene.String()
+    message = graphene.Field(lambda: MessageType)
+
+    @staticmethod
+    def mutate(self, info, message):
+        if not info.context.user.is_authenticated:
+            print('14')
+            return CreateMessage(form_errors=json.dumps('Please login!'))
+        message = models.Message.objects.create(
+            user=info.context.user, message=message)
+        return CreateMessage(message=message, form_errors=None)
+
+
+class Mutation(graphene.AbstractType):
+    create_message = CreateMessage.Field()
 
 
 class Query(graphene.AbstractType):
