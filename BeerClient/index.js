cf9356076a847f87d477d756f6915e1f4ae4dca1
Florn <flapsnms@gmail.com>
2018-08-31 20:05:09 +0100

    add apollo dependencies, add ApolloProvider to index.js

--- BeerClient/index.js
+++ BeerClient/index.js
@@ -1,7 +1,23 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+// import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
-AppRegistry.registerComponent(appName, () => App);
+// Create the client as outlined in the setup guide
+// const client = new ApolloClient();
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/"
+});
+
+const AppRoot = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent("BeerClient", () => App);
