46a6d38d87af872fa37516062d63120392218a24
Florn <flapsnms@gmail.com>
2018-11-18 00:20:30 +0000

    add Profile component

---
+++ BeerClient/modules/profile/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+export default class Profile extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Profile</Text>
+      </View>
+    );
+  }
+}
